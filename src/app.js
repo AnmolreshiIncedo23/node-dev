feffc5db60e748ada1f620ccd2735e19b7f24b78
AnmolreshiIncedo23 <anmol@foneapp.com>
2025-01-16 11:12:18 +0530

    explore get post

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ console.log("Starting new connection");
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({firstName:"Anmol",lastName:"reshi"});
+})
+
 app.use("/test", (req, res) => {
   res.send("hello from the test server");
 });
 
-app.use("/", (req, res) => {
-  res.send("hello from main server");
-});
-app.use("/t", (req, res) => {
-  res.send("hello from test server");
-});
+// app.use("/t", (req, res) => {
+//   res.send("hello from test server");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("hello from main server");
+// });
 
 // app.listen(3000);// listen on port 3000
 app.listen(3000, () => {
